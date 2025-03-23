1031595635da95094ac8568e080d279fee71153c
AMfalme <mfalmegriffin@gmail.com>
2025-03-23 13:07:56 +0300

    . F ensure only logged in users can visit the dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,16 +1,27 @@
-import { AppSidebar } from "@/components/app-sidebar"
-import { ChartAreaInteractive } from "@/components/chart-area-interactive"
-import { DataTable } from "@/components/data-table"
-import { SectionCards } from "@/components/section-cards"
-import { SiteHeader } from "@/components/site-header"
-import {
-  SidebarInset,
-  SidebarProvider,
-} from "@/components/ui/sidebar"
+"use client";
+import { AppSidebar } from "@/components/app-sidebar";
+import { ChartAreaInteractive } from "@/components/chart-area-interactive";
+import { DataTable } from "@/components/data-table";
+import { SectionCards } from "@/components/section-cards";
+import { SiteHeader } from "@/components/site-header";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 
-import data from "./data.json"
+import { useAuth } from "@/components/providers/auth-provider";
+
+import data from "./data.json";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Page() {
+  const { user, loading } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!loading && !user) router.push("/login");
+  }, [user, loading, router]);
+
+  if (loading) return <p>Loading...</p>;
+  if (!user) return null;
   return (
     <SidebarProvider
       style={
@@ -36,5 +47,5 @@ export default function Page() {
         </div>
       </SidebarInset>
     </SidebarProvider>
-  )
+  );
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -6,12 +6,13 @@ import { ModeToggle } from "./ui/mode-toggle";
 import Link from "next/link";
 import { useAuth } from "./providers/auth-provider";
 import { getAuth, signOut } from "firebase/auth";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
+  const pathName = usePathname();
   const { user } = useAuth();
   const auth = getAuth();
-  console.log(user, "user in navbar");
-  console.log(auth, "auth in navbar");
+  console.log(pathName, "pathname");
   return (
     <nav className="w-full px-6 py-4 bg-background border-b border-border flex items-center justify-between">
       <Link href="/" className="text-xl font-bold text-primary">
@@ -19,12 +20,19 @@ export default function Navbar() {
       </Link>
 
       <div className="flex items-center gap-4">
-        <Link href="/catalogue" className="text-sm font-medium hover:underline">
+        <Link
+          href="/catalogue"
+          className={
+            pathName == "/catalogue"
+              ? "text-sm font-medium hover:underline text-gray-500"
+              : "text-sm font-medium hover:underline"
+          }
+        >
           Shop
         </Link>
-        {/* <Link href="/dashboard" className="text-sm font-medium hover:underline">
+        <Link href="/dashboard" className="text-sm font-medium hover:underline">
           Dashboard
-        </Link> */}
+        </Link>
         {user ? (
           <div className="flex items-center gap-4">
             <Button variant="outline" onClick={() => signOut(auth)}>
