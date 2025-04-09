6c02c4d80c3c62ddc002edbeacaad062eb549b1a
AMfalme <mfalmegriffin@gmail.com>
2025-04-09 13:35:38 +0300

    . R add tawk.to

--- src/app/whatsapp-button.tsx
+++ src/app/whatsapp-button.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { Button } from "@headlessui/react"; // Adjust the import path based on your library or project setup
+import { useEffect } from "react";
 
 const WhatsAppButton = () => {
   const handleSubmit = () => {
@@ -10,6 +11,14 @@ const WhatsAppButton = () => {
     // Open the WhatsApp chat
     window.open(whatsappUrl, "_blank");
   };
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://embed.tawk.to//67f64617d640c5190a38c023/1iod03jh4";
+    script.async = true;
+    script.charset = "UTF-8";
+    script.setAttribute("crossorigin", "*");
+    document.body.appendChild(script);
+  }, []);
 
   return (
     <Button className={"whatsapp-button"} onClick={handleSubmit}>
