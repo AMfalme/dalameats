668a3972287b818dddc06c5245864eae43395686
AMfalme <mfalmegriffin@gmail.com>
2025-10-16 23:32:44 +0300

    fix db import

--- src/components/user.ts
+++ src/components/user.ts
@@ -1,5 +1,5 @@
 import { doc, setDoc } from "firebase/firestore";
-import { db } from "@/lib/firebase";
+import { db } from "@/lib/firebase/config";
 import { userDetails } from "@/types/user";
 export const saveUserDetails = async (uid: string, data: userDetails) => {
   await setDoc(doc(db, "users", uid), data, { merge: true });
